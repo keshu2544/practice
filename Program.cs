057616b57e13b80de5e168ef0f00a8c06068526d
keshu soman <keshusoman29@gmail.com>
2023-04-14 16:58:28 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Helloword
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+          string name;
+          Console.WriteLine ("what is your name");
+          name = (Console.ReadLine());
+          Console.WriteLine("hello "+ name);
+        
+    }
+}
+}
